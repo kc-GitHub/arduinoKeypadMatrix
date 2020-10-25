c17971ca269619640d97a32a337f114838ed1a08
Dirk Hoffmann <hoffmann@vmd-jena.de>
2020-10-25 14:58:50 +0100

    Changed indentation

--- KeyPadMatrix.cpp
+++ KeyPadMatrix.cpp
@@ -8,29 +8,36 @@
 /***********************************************************************************************************************
  */
 KeyPadMatrix::KeyPadMatrix(uint8 row0, uint8 row1, uint8 row2, uint8 row3) {
-	this->pinMap[0] = row0;
-	this->pinMap[1] = row1;
-	this->pinMap[2] = row2;
-	this->pinMap[3] = row3;
+    pinMap[0] = row0;
+    pinMap[1] = row1;
+    pinMap[2] = row2;
+    pinMap[3] = row3;
+}
+
+KeyPadMatrix::KeyPadMatrix(uint8 row0, uint8 row1, uint8 row2) {
+    pinMap[0] = row0;
+    pinMap[1] = row1;
+    pinMap[2] = row2;
+    rowCountMax = 3;
 }
 
 /***********************************************************************************************************************
  */
 
-void KeyPadMatrix::scanKeyPad(void) {
-	uint8_t keyCodeLast = 0;
+void KeyPadMatrix::scan(void) {
+    uint8_t keyCodeLast = 0;
 
-	// Configure scan row pins
-	for (uint8_t i = 0; i < 4; i++) {
-		if (i != scanRow || scanRow == 4) {
-			// Set row as input
-			pinMode(pinMap[i], INPUT_PULLUP);
-		} else {
-			// Set row as output an low
-			pinMode(pinMap[i], OUTPUT);
-			digitalWrite(pinMap[i], LOW);
-		}
-	}
+    // Configure scan row pins
+    for (uint8_t i = 0; i < rowCountMax; i++) {
+        if (i != scanRow || scanRow == rowCountMax) {
+            // Set row as input
+            pinMode(pinMap[i], INPUT_PULLUP);
+        } else {
+            // Set row as output an low
+            pinMode(pinMap[i], OUTPUT);
+            digitalWrite(pinMap[i], LOW);
+        }
+    }
 
 	// Read all row pins
 	uint8_t keyByte = !digitalRead(pinMap[0]) << 0 | !digitalRead(pinMap[1]) << 1 | !digitalRead(pinMap[2]) << 2 | !digitalRead(pinMap[3]) << 3;
@@ -41,52 +48,52 @@ void KeyPadMatrix::scanKeyPad(void) {
 	// Calculate the key code
 	uint8_t keycode = (keyByte * (1 << (scanRow))) + scanRow;
 
-	if (keyByte > 0 && scanRow == 4) {
-		keyCodeLast = keycode;
-	} else if (keyByte > 0 && this->keyCodeSaved == 0) {
-		this->keyCodeSaved = keycode;
-	}
+    if (keyByte > 0 && scanRow == rowCountMax) {
+        keyCodeLast = keycode;
+    } else if (keyByte > 0 && keyCodeSaved == 0) {
+        keyCodeSaved = keycode;
+    }
 
-	scanRow ++;
+    scanRow ++;
 
-	if (scanRow > 4) {
-		scanRow = 0;
+    if (scanRow > rowCountMax) {
+        scanRow = 0;
 
-		if (this->keyCodeSaved || keyCodeLast) {
-			this->keyCode = keyCodeLast ? keyCodeLast : this->keyCodeSaved;
-			this->keyCodeSaved = 0;
-			keyCodeLast = 0;
-		} else {
-			this->keyCode = 0;
-		}
+        if (keyCodeSaved || keyCodeLast) {
+            keyCode = keyCodeLast ? keyCodeLast : keyCodeSaved;
+            keyCodeSaved = 0;
+            keyCodeLast = 0;
+        } else {
+            keyCode = 0;
+        }
 
-		if (keyCodeOld != this->keyCode && keyCodeChanged == 0) {
-			if (keyCodeOld == 0 && this->keyCode > 0) {
-				mode = KEYPADMATRIX_MODE_PRESSED;
-			} else if (this->keyCode == 0 && keyCodeOld > 0) {
-				mode = KEYPADMATRIX_MODE_RELEASED;
-			} else {
-				mode = KEYPADMATRIX_MODE_NONE;
-			}
+        if (keyCodeOld != keyCode && keyCodeChanged == 0) {
+            if (keyCodeOld == 0 && keyCode > 0) {
+                mode = KEYPADMATRIX_MODE_PRESSED;
+            } else if (keyCode == 0 && keyCodeOld > 0) {
+                mode = KEYPADMATRIX_MODE_RELEASED;
+            } else {
+                mode = KEYPADMATRIX_MODE_NONE;
+            }
 
-			if (mode) {
-				keyCodeChanged = 1;
-				keyCodeOld = this->keyCode;
-			}
-		}
-	}
+          if (mode) {
+                keyCodeChanged = 1;
+                keyCodeOld = keyCode;
+          }
+        }
+    }
 }
 
 uint8 KeyPadMatrix::keyCodeHasChanged (void) {
-	return keyCodeChanged;
+    return keyCodeChanged;
 }
 
 uint8 KeyPadMatrix::getMode (void) {
-	return mode;
+    return mode;
 }
 
 uint8 KeyPadMatrix::getKeycode() {
-	uint8 keycode = this->keyCode;
-	keyCodeChanged = 0;
-	return keycode;
+    uint8 keycode = keyCode;
+    keyCodeChanged = 0;
+    return keycode;
 }
--- KeyPadMatrix.h
+++ KeyPadMatrix.h
@@ -10,31 +10,34 @@
 
 #include <Arduino.h>
 
-#define KEYPADMATRIX_MODE_NONE		0
-#define KEYPADMATRIX_MODE_PRESSED	1
-#define KEYPADMATRIX_MODE_RELEASED	2
+#define KEYPADMATRIX_MODE_NONE        0
+#define KEYPADMATRIX_MODE_PRESSED     1
+#define KEYPADMATRIX_MODE_RELEASED    2
 
 class KeyPadMatrix {
-	public:
-		/* Constructor */
-		KeyPadMatrix(uint8 row0, uint8 row1, uint8 row2, uint8 row3);
+    public:
+        /* Constructor */
+        KeyPadMatrix(uint8 row0, uint8 row1, uint8 row2, uint8 row3);
+        KeyPadMatrix(uint8 row0, uint8 row1, uint8 row2);
 
-		void scanKeyPad();
-		uint8 keyCodeHasChanged(void);
-		uint8 getKeycode(void);
-		uint8 getMode(void);
+        void scan();
+        uint8 keyCodeHasChanged(void);
+        uint8 getKeycode(void);
+        uint8 getMode(void);
 
-	private:
-		uint8 pinMap[4];
-		uint8_t scanRow = 0;		// counter for current scan row
+    private:
+        uint8_t rowCountMax = 4;
+        uint8_t pinMap[4];
+        uint8_t scanRow = 0;		// counter for current scan row
+        uint8_t setupDone = 0;
 
+        uint8_t keyCode = 0;
+        uint8_t keyCodeOld = 255;
+        uint8_t keyCodeSaved = 0;
 
-		uint8_t keyCode = 0;
-		uint8_t keyCodeOld = 255;
-		uint8_t keyCodeSaved = 0;
-
-		uint8_t keyCodeChanged = 0;
-		uint8_t mode = 0;
-	};
+        uint8_t keyCodeChanged = 0;
+        uint8_t mode = 0;
+        uint8_t keyCode2 = 0;
+    };
 
 #endif
